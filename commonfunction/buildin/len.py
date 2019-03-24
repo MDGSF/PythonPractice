1529dd9b2f76d585f0d9e384ae18c12c9f6c73b0
huangjian <1342042894@qq.com>
2019-03-24 18:08:49 +0800

    add len

---
+++ commonfunction/buildin/len.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+# len(s)
+#     Return the length (the number of items) of an object. The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
+
+
+def main():
+    l = [1, 2, 3, 4]
+    print(len(l))
+
+    s = "huangjian"
+    print(len(s))
+
+
+if __name__ == "__main__":
+    main()
