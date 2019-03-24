30409251aae7f7c5adc078d89bcd6e38e2ca6c47
huangjian <1342042894@qq.com>
2019-03-24 18:21:01 +0800

    add ord

---
+++ commonfunction/buildin/ord.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+# ord(c)
+#     Given a string representing one Unicode character, return an integer representing the Unicode code point of that character. For example, ord('a') returns the integer 97 and ord('€') (Euro sign) returns 8364. This is the inverse of chr().
+
+
+def main():
+    print(ord("a"))
+    print(ord('A'))
+
+
+if __name__ == "__main__":
+    main()
