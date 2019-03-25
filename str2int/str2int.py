bf8679199b55cb6777ecdb20c7677822839dae9a
huangjian <1342042894@qq.com>
2019-03-25 18:52:16 +0800

    modify str2int

--- str2int/str2int.py
+++ str2int/str2int.py
@@ -6,12 +6,8 @@ DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
           '8': 8, '9': 9}
 
 
-def char2num(c):
-    return DIGITS[c]
-
-
 def str2int(s):
-    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+    return reduce(lambda x, y: x * 10 + y, map(lambda c: DIGITS[c], s))
 
 
 def main():
