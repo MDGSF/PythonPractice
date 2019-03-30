7e83e4b4b9ab243ab87f2fe741828f14ac723fdb
huangjian <1342042894@qq.com>
2019-03-30 15:06:48 +0800

    add string demo

---
+++ commonfunction/string/string_demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import string
+
+
+def main():
+    print(string.ascii_letters)
+    print(string.ascii_lowercase)
+    print(string.ascii_uppercase)
+    print(string.digits)
+    print(string.hexdigits)
+    print(string.octdigits)
+    print(string.punctuation)
+    print(string.printable)
+
+
+if __name__ == "__main__":
+    main()
