c8f90d0c6cada1b89cc59051a1430727ba3be7df
huangjian <1342042894@qq.com>
2019-04-04 16:48:29 +0800

    add regex

---
+++ regex/README.md
@@ -0,0 +1,7 @@
+# regex
+
+https://docs.python.org/3/library/re.html
+
+https://regex101.com/
+
+https://regexper.com/
--- test/test.py
+++ test/test.py
@@ -6,8 +6,7 @@ def main():
     l = [0,1,2,3,4,5]
     print(l)
 
-    l[1:6:2] = [100, 200, 300]
-    print(l)
+    l.insert([], 10)
 
 if __name__ == "__main__":
     main()
