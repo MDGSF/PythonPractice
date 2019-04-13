bcf6969b3bd759aaa47ce1552e43697e50243a19
huangjian <1342042894@qq.com>
2019-04-13 15:30:23 +0800

    add regex

--- re/redemo.py
+++ re/redemo.py
@@ -14,8 +14,34 @@ def test1():
         print(match.group())
 
 
+def test2():
+    print('\ntest2')
+
+    pattern = re.compile(r'(?P<quote>---)(.*)(?P=quote)',
+                         flags=re.DOTALL)
+    strs = r"""
+    
+---
+layout: post
+title: "[字符串] 全排列"
+date: 2016-04-07
+author: mdgsf
+comments: true
+categories: Art
+tags: [C,String,Permutation]
+description:
+published: true #default true
+---
+    
+    """
+
+    result = pattern.findall(strs)
+    print(result)
+
+
 def main():
     test1()
+    test2()
 
 
 if __name__ == "__main__":
