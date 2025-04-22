22db701d148b278692f82c27c424ee2cc43c1062
huangjian <huangjian@bicv.com>
2025-04-22 20:20:24 +0800

    feat: update fastapi demo24

--- fastapi/demo24/main.py
+++ fastapi/demo24/main.py
@@ -10,3 +10,14 @@ async def read_item(item_id: str):
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
     return {"item": items[item_id]}
+
+
+@app.get("/items-header/{item_id}")
+async def read_item_header(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
