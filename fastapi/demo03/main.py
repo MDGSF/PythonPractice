b9e3bfc543d890edf68ed5b72efc3466a761e2ab
huangjian <huangjian@bicv.com>
2025-04-22 10:31:56 +0800

    feat: add fastapi demo03

---
+++ fastapi/demo03/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# 声明的参数不是路径参数时，路径操作函数会把该参数自动解释为查询参数。
+@app.get("/items/")
+async def read_item_01(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item_02(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
